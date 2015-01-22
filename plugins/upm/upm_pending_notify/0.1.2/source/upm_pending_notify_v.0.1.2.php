ec4082d0eff9fb477c892d5bec652ec3894f6850
Pete Cooper <pete@pete-mbp17.local>
2015-01-22 15:21:11 +0000

    Fix source code so it's actually source code
    Was previously a decoded jumble of code and help text. Hat tip and
    thanks: @gocom

--- plugins/upm/upm_pending_notify/0.1.2/source/upm_pending_notify_v.0.1.2.php
+++ plugins/upm/upm_pending_notify/0.1.2/source/upm_pending_notify_v.0.1.2.php
@@ -1,226 +1,226 @@
-a:11:{s:4:"name";s:18:"upm_pending_notify";s:11:"description";s:29:"Pending article notification.";s:7:"version";s:5:"0.1.2";s:4:"type";i:1;s:6:"author";s:13:"Mary Fredborg";s:10:"author_uri";s:52:"http://utterplush.com/txp-plugins/upm-pending-notify";s:4:"code";s:5831:"if (txpinterface == 'admin')
-	{
-		// i quite hate doing it this way, the write tab's setup is screwy
+<?phpif (txpinterface == 'admin')
+    {
+        // i quite hate doing it this way, the write tab's setup is screwy
 
-		add_privs('upm_pending_notify_created', '1,2,3,4,5');
-		register_callback('upm_pending_notify_created', 'article', 'edit');
+        add_privs('upm_pending_notify_created', '1,2,3,4,5');
+        register_callback('upm_pending_notify_created', 'article', 'edit');
 
-		add_privs('upm_pending_notify_updated_pre', '1,2,3,4,5');
-		register_callback('upm_pending_notify_updated_pre', 'article', '', 1);
+        add_privs('upm_pending_notify_updated_pre', '1,2,3,4,5');
+        register_callback('upm_pending_notify_updated_pre', 'article', '', 1);
 
-		add_privs('upm_pending_notify_updated_post', '1,2,3,4,5');
-		register_callback('upm_pending_notify_updated_post', 'article', 'edit');
-	}
-
-//--------------------------------------------------------------
-
-	function upm_pending_notify_created()
-	{
-		global $ID;
-
-		// not trying to create a new article
-		// begone!
-		if (!gps('publish'))
-		{
-			return;
-		}
-
-		$Status = (int) ps('Status');
-
-		if (!has_privs('article.publish') && $Status >= 4)
-		{
-			$Status = 3;
-		}
-
-		if ($Status == 3)
-		{
-			$ID	= (int) $ID;
-			$Title = ps('Title');
-
-			upm_pending_notify_publisher($ID, $Title);
-		}
-	}
+        add_privs('upm_pending_notify_updated_post', '1,2,3,4,5');
+        register_callback('upm_pending_notify_updated_post', 'article', 'edit');
+    }
 
 //--------------------------------------------------------------
 
-	function upm_pending_notify_updated_pre()
-	{
-		$GLOBALS['upm_pending_notify'] = false;
+    function upm_pending_notify_created()
+    {
+        global $ID;
 
-		// article updated
-		if (gps('save'))
-		{
-			$ID = (int) ps('ID');
+        // not trying to create a new article
+        // begone!
+        if (!gps('publish'))
+        {
+            return;
+        }
 
-			$Status = (int) ps('Status');
-			$old_status = (int) safe_field('Status', 'textpattern', "ID = $ID");
+        $Status = (int) ps('Status');
 
-			// only notify if...
+        if (!has_privs('article.publish') && $Status >= 4)
+        {
+            $Status = 3;
+        }
 
-			// ...marked as pending
-			// publishers are notified
-			// see: upm_pending_notify_updated_post()
-			if ($Status == 3)
-			{
-				$GLOBALS['upm_pending_notify'] = 'publishers';
-			}
+        if ($Status == 3)
+        {
+            $ID    = (int) $ID;
+            $Title = ps('Title');
 
-			// ...marked as live/sticky and was previously pending
-			// author is notified
-			// see: upm_pending_notify_updated_post()
-			elseif ($Status > 3 and $old_status == 3)
-			{
-				$GLOBALS['upm_pending_notify'] = 'author';
-			}
-		}
-	}
+            upm_pending_notify_publisher($ID, $Title);
+        }
+    }
 
 //--------------------------------------------------------------
 
-	function upm_pending_notify_updated_post()
-	{
-		global $upm_pending_notify, $txp_user, $prefs;
+    function upm_pending_notify_updated_pre()
+    {
+        $GLOBALS['upm_pending_notify'] = false;
 
-		if (!$upm_pending_notify)
-		{
-			return;
-		}
+        // article updated
+        if (gps('save'))
+        {
+            $ID = (int) ps('ID');
 
-		$ID = (int) ps('ID');
-		$Title = ps('Title');
+            $Status = (int) ps('Status');
+            $old_status = (int) safe_field('Status', 'textpattern', "ID = $ID");
 
-		switch ($upm_pending_notify)
-		{
-			case 'publishers':
-				upm_pending_notify_publisher($ID, $Title);
-			break;
+            // only notify if...
 
-			case 'author':
-				$AuthorID = ps('AuthorID');
-				$LastModID = ps('LastModID');
+            // ...marked as pending
+            // publishers are notified
+            // see: upm_pending_notify_updated_post()
+            if ($Status == 3)
+            {
+                $GLOBALS['upm_pending_notify'] = 'publishers';
+            }
 
-				upm_pending_notify_author($ID, $Title, $AuthorID, $LastModID);
-			break;
-		}
-	}
-
-//--------------------------------------------------------------
-
-	function upm_pending_notify_publisher($ID, $Title)
-	{
-		global $txp_user, $prefs;
-
-		$publishers = safe_rows_start('RealName, email', 'txp_users', "privs = 1 and name != '".doSlash($txp_user)."'");
-
-		if ($publishers)
-		{
-			$Title = ($Title) ? strip_tags($Title) : gTxt('untitled');
-			$author = get_author_name($txp_user);
-
-			$subject = upm_pending_notify_gTxt('email_subject_publisher', array(
-				'{sitename}' => $prefs['sitename'],
-				'{title}'		 => $Title,
-			));
-
-			while ($publisher = nextRow($publishers))
-			{
-				$body = upm_pending_notify_gTxt('email_message_publisher', array(
-					'{article_url}' => hu.'textpattern/index.php?event=article&step=edit&ID='.$ID,
-					'{author}'			=> $author,
-					'{publisher}'		=> $publisher['RealName'],
-					'{title}'				=> $Title,
-				));
-
-				upm_pending_notify_mail($publisher['RealName'], $publisher['email'], $subject, $body);
-			}
-		}
-	}
+            // ...marked as live/sticky and was previously pending
+            // author is notified
+            // see: upm_pending_notify_updated_post()
+            elseif ($Status > 3 and $old_status == 3)
+            {
+                $GLOBALS['upm_pending_notify'] = 'author';
+            }
+        }
+    }
 
 //--------------------------------------------------------------
 
-	function upm_pending_notify_author($ID, $Title, $AuthorID, $LastModID)
-	{
-		global $txp_user, $prefs;
+    function upm_pending_notify_updated_post()
+    {
+        global $upm_pending_notify, $txp_user, $prefs;
 
-		$author = safe_row('RealName, email', 'txp_users', "name = '".doSlash($AuthorID)."' and name != '".doSlash($txp_user)."'");
+        if (!$upm_pending_notify)
+        {
+            return;
+        }
 
-		if ($author)
-		{
-			include_once txpath.'/publish/taghandlers.php';
+        $ID = (int) ps('ID');
+        $Title = ps('Title');
 
-			$Title = ($Title) ? strip_tags($Title) : gTxt('untitled');
-			$url = permlinkurl_id($ID);
-			$publisher = get_author_name($txp_user);
+        switch ($upm_pending_notify)
+        {
+            case 'publishers':
+                upm_pending_notify_publisher($ID, $Title);
+            break;
 
-			$subject = upm_pending_notify_gTxt('email_subject_author', array(
-				'{sitename}' => $prefs['sitename'],
-				'{title}'		 => $Title,
-			));
+            case 'author':
+                $AuthorID = ps('AuthorID');
+                $LastModID = ps('LastModID');
 
-			$body = upm_pending_notify_gTxt('email_message_author', array(
-				'{article_url}' => $url,
-				'{author}'			=> $author['RealName'],
-				'{publisher}'		=> $publisher,
-				'{title}'				=> $Title,
-			));
-
-			upm_pending_notify_mail($author['RealName'], $author['email'], $subject, $body);
-		}
-	}
+                upm_pending_notify_author($ID, $Title, $AuthorID, $LastModID);
+            break;
+        }
+    }
 
 //--------------------------------------------------------------
 
-	function upm_pending_notify_mail($name, $email, $subject, $body)
-	{
-		global $prefs;
+    function upm_pending_notify_publisher($ID, $Title)
+    {
+        global $txp_user, $prefs;
 
-		if ($prefs['override_emailcharset'])
-		{
-			$charset = 'ISO-8859-1';
+        $publishers = safe_rows_start('RealName, email', 'txp_users', "privs = 1 and name != '".doSlash($txp_user)."'");
 
-			if (is_callable('utf8_decode'))
-			{
-				$name		 = utf8_decode($name);
-				$email	 = utf8_decode($email);
+        if ($publishers)
+        {
+            $Title = ($Title) ? strip_tags($Title) : gTxt('untitled');
+            $author = get_author_name($txp_user);
 
-				$subject = utf8_decode($subject);
-				$body		 = utf8_decode($body);
-			}
-		}
+            $subject = upm_pending_notify_gTxt('email_subject_publisher', array(
+                '{sitename}' => $prefs['sitename'],
+                '{title}'         => $Title,
+            ));
 
-		else
-		{
-			$charset = 'UTF-8';
-		}
+            while ($publisher = nextRow($publishers))
+            {
+                $body = upm_pending_notify_gTxt('email_message_publisher', array(
+                    '{article_url}' => hu.'textpattern/index.php?event=article&step=edit&ID='.$ID,
+                    '{author}'            => $author,
+                    '{publisher}'        => $publisher['RealName'],
+                    '{title}'                => $Title,
+                ));
 
-		$name = encode_mailheader(strip_rn($name), 'phrase');
-		$email = strip_rn($email);
-
-		$subject = encode_mailheader(strip_rn($subject), 'text');
-
-		$sep = !is_windows() ? "\n" : "\r\n";
-
-		$body = str_replace("\r\n", "\n", $body);
-		$body = str_replace("\r", "\n", $body);
-		$body = str_replace("\n", $sep, $body);
-
-		return mail("$name <$email>", $subject, $body,
-			"From: $name <$email>".
-			$sep.'X-Mailer: upm_pending_notify Textpattern plugin'.
-			$sep.'Content-Transfer-Encoding: 8bit'.
-			$sep.'Content-Type: text/plain; charset="'.$charset.'"'.
-			$sep
-		);
-	}
+                upm_pending_notify_mail($publisher['RealName'], $publisher['email'], $subject, $body);
+            }
+        }
+    }
 
 //--------------------------------------------------------------
 
-	function upm_pending_notify_gTxt($what, $vars = array())
-	{
-		$lang = array();
+    function upm_pending_notify_author($ID, $Title, $AuthorID, $LastModID)
+    {
+        global $txp_user, $prefs;
 
-		$lang['email_subject_author']		 = '[{sitename}] Article published: {title}';
-		$lang['email_message_author']		 = <<<eml
+        $author = safe_row('RealName, email', 'txp_users', "name = '".doSlash($AuthorID)."' and name != '".doSlash($txp_user)."'");
+
+        if ($author)
+        {
+            include_once txpath.'/publish/taghandlers.php';
+
+            $Title = ($Title) ? strip_tags($Title) : gTxt('untitled');
+            $url = permlinkurl_id($ID);
+            $publisher = get_author_name($txp_user);
+
+            $subject = upm_pending_notify_gTxt('email_subject_author', array(
+                '{sitename}' => $prefs['sitename'],
+                '{title}'         => $Title,
+            ));
+
+            $body = upm_pending_notify_gTxt('email_message_author', array(
+                '{article_url}' => $url,
+                '{author}'            => $author['RealName'],
+                '{publisher}'        => $publisher,
+                '{title}'                => $Title,
+            ));
+
+            upm_pending_notify_mail($author['RealName'], $author['email'], $subject, $body);
+        }
+    }
+
+//--------------------------------------------------------------
+
+    function upm_pending_notify_mail($name, $email, $subject, $body)
+    {
+        global $prefs;
+
+        if ($prefs['override_emailcharset'])
+        {
+            $charset = 'ISO-8859-1';
+
+            if (is_callable('utf8_decode'))
+            {
+                $name         = utf8_decode($name);
+                $email     = utf8_decode($email);
+
+                $subject = utf8_decode($subject);
+                $body         = utf8_decode($body);
+            }
+        }
+
+        else
+        {
+            $charset = 'UTF-8';
+        }
+
+        $name = encode_mailheader(strip_rn($name), 'phrase');
+        $email = strip_rn($email);
+
+        $subject = encode_mailheader(strip_rn($subject), 'text');
+
+        $sep = !is_windows() ? "\n" : "\r\n";
+
+        $body = str_replace("\r\n", "\n", $body);
+        $body = str_replace("\r", "\n", $body);
+        $body = str_replace("\n", $sep, $body);
+
+        return mail("$name <$email>", $subject, $body,
+            "From: $name <$email>".
+            $sep.'X-Mailer: upm_pending_notify Textpattern plugin'.
+            $sep.'Content-Transfer-Encoding: 8bit'.
+            $sep.'Content-Type: text/plain; charset="'.$charset.'"'.
+            $sep
+        );
+    }
+
+//--------------------------------------------------------------
+
+    function upm_pending_notify_gTxt($what, $vars = array())
+    {
+        $lang = array();
+
+        $lang['email_subject_author']         = '[{sitename}] Article published: {title}';
+        $lang['email_message_author']         = <<<eml
 Dear {author},
 
 {publisher} has published your article:
@@ -229,8 +229,8 @@ Dear {author},
 {article_url}
 eml;
 
-		$lang['email_subject_publisher'] = '[{sitename}] Article submitted: {title}';
-		$lang['email_message_publisher'] = <<<eml
+        $lang['email_subject_publisher'] = '[{sitename}] Article submitted: {title}';
+        $lang['email_message_publisher'] = <<<eml
 Dear {publisher},
 
 {author} has submitted an article for review:
@@ -239,37 +239,5 @@ Dear {publisher},
 {article_url}
 eml;
 
-		return strtr($lang[$what], $vars);
-	}";s:4:"help";s:1193:"h1. upm_pending_notify
-
-h2. What this plugin does:
-
-# When someone creates an article with the status "Pending", all the "Publisher" users receive an email. This email contains very basic information of the article, and link to further view and manage the article. If the author happens to be a Publisher, he does not receive the email.
-# When an article is changed from "Pending" to "Live" status, the article's author receives a link to view their article on the live site. If the author happens to be a Publisher *and* set the article to "Live" herself, she does not receive the email.
-
-h2. What this plugin *does not* do:
-
-# Author notifications are *not* sent out when article status is changed with the multi-edit feature of the Articles tab. For technical reasons, this could too easily cause grand problems with your site and make your web host very upset with you.
-# Allow all kinds of customizations to the emails sent concerning which article fields are included in the email for preview. The plugin remains nice and lighter-weight by providing only the essential information.
-
-h2. How to use it:
-
-# Install the plugin (which you've already done).
-# Activate the plugin.
-# All done.";s:8:"help_raw";s:1193:"h1. upm_pending_notify
-
-h2. What this plugin does:
-
-# When someone creates an article with the status "Pending", all the "Publisher" users receive an email. This email contains very basic information of the article, and link to further view and manage the article. If the author happens to be a Publisher, he does not receive the email.
-# When an article is changed from "Pending" to "Live" status, the article's author receives a link to view their article on the live site. If the author happens to be a Publisher *and* set the article to "Live" herself, she does not receive the email.
-
-h2. What this plugin *does not* do:
-
-# Author notifications are *not* sent out when article status is changed with the multi-edit feature of the Articles tab. For technical reasons, this could too easily cause grand problems with your site and make your web host very upset with you.
-# Allow all kinds of customizations to the emails sent concerning which article fields are included in the email for preview. The plugin remains nice and lighter-weight by providing only the essential information.
-
-h2. How to use it:
-
-# Install the plugin (which you've already done).
-# Activate the plugin.
-# All done.";s:15:"allow_html_help";i:0;s:3:"md5";s:32:"17660e2018477965d301383b7a2a65a4";}
+        return strtr($lang[$what], $vars);
+    }?>
--- plugins/upm/upm_savenew/0.4.2/source/upm_savenew_v0.4.2.php
+++ plugins/upm/upm_savenew/0.4.2/source/upm_savenew_v0.4.2.php
@@ -1,85 +1,77 @@
-a:11:{s:4:"name";s:11:"upm_savenew";s:11:"description";s:41:""Save New" button for articles and forms.";s:7:"version";s:5:"0.4.2";s:4:"type";i:1;s:6:"author";s:13:"Mary Fredborg";s:10:"author_uri";s:45:"http://utterplush.com/txp-plugins/upm-savenew";s:4:"code";s:1975:"if (txpinterface == 'admin')
-	{
-		// link to javascript
-		add_privs('upm_savenew_js_link_load', '1,2,3,4,5,6');
-		register_callback('upm_savenew_js_link_load', 'article', 'edit', 1);
-		register_callback('upm_savenew_js_link_load', 'form', '', 1);
+<?phpif (txpinterface == 'admin')
+    {
+        // link to javascript
+        add_privs('upm_savenew_js_link_load', '1,2,3,4,5,6');
+        register_callback('upm_savenew_js_link_load', 'article', 'edit', 1);
+        register_callback('upm_savenew_js_link_load', 'form', '', 1);
 
-		// load javascript
-		add_privs('upm_savenew_js', '1,2,3,4,5,6');
-		register_callback('upm_savenew_js', 'upm_savenew_js', '', 1);
-	}
-
-// -------------------------------------------------------------
-
-	function upm_savenew_js_link_load()
-	{
-		ob_start('upm_savenew_js_link');
-	}
-
-	function upm_savenew_js_link($buffer)
-	{
-		$find = '</head>';
-		$replace = n.n.t.t.'<script type="text/javascript" src="index.php?event=upm_savenew_js"></script>'.n.t;
-
-		return str_replace($find, $replace.$find, $buffer);
-	}
+        // load javascript
+        add_privs('upm_savenew_js', '1,2,3,4,5,6');
+        register_callback('upm_savenew_js', 'upm_savenew_js', '', 1);
+    }
 
 // -------------------------------------------------------------
 
-	function upm_savenew_js()
-	{
-		while (@ob_end_clean());
+    function upm_savenew_js_link_load()
+    {
+        ob_start('upm_savenew_js_link');
+    }
 
-		$save_new = gTxt('save_new');
+    function upm_savenew_js_link($buffer)
+    {
+        $find = '</head>';
+        $replace = n.n.t.t.'<script type="text/javascript" src="index.php?event=upm_savenew_js"></script>'.n.t;
 
-		header("Content-type: text/javascript");
+        return str_replace($find, $replace.$find, $buffer);
+    }
 
-		echo <<<js
+// -------------------------------------------------------------
+
+    function upm_savenew_js()
+    {
+        while (@ob_end_clean());
+
+        $save_new = gTxt('save_new');
+
+        header("Content-type: text/javascript");
+
+        echo <<<js
 /*
 upm_savenew
 */
 
-	$(document).ready(function() {
-		// create new article submit button
-		$('#page-article input[name="save"]').
-			after(' <input type="submit" name="publish" value="$save_new" class="publish" />');
+    $(document).ready(function() {
+        // create new article submit button
+        $('#page-article input[name="save"]').
+            after(' <input type="submit" name="publish" value="$save_new" class="publish" />');
 
-		// article save new button
-		$('#page-article input[name="save"] + input[name="publish"]').
-			// onclick...
-			click(function(){
-				// check reset time checkbox
-				$('#reset_time').attr({
-					name: 'publish_now',
-					checked: true
-				});
+        // article save new button
+        $('#page-article input[name="save"] + input[name="publish"]').
+            // onclick...
+            click(function(){
+                // check reset time checkbox
+                $('#reset_time').attr({
+                    name: 'publish_now',
+                    checked: true
+                });
 
-				// empty URL-only title
-				$('#url-title').attr('value', '');
-			});
+                // empty URL-only title
+                $('#url-title').attr('value', '');
+            });
 
-		// create new form submit button
-		$('#page-form input[name="save"]').
-			after(' <input type="submit" name="savenew" value="$save_new" class="publish" />');
+        // create new form submit button
+        $('#page-form input[name="save"]').
+            after(' <input type="submit" name="savenew" value="$save_new" class="publish" />');
 
-		// forms save new button
-		$('#page-form input[name="save"] + input[name="savenew"]').
-			// onclick...
-			click(function(){
-				// change form name from original to original_copy
-				$('input[name="name"]').attr('value', $('input[name="name"]').attr('value') + '_copy');
-			});
-	});
+        // forms save new button
+        $('#page-form input[name="save"] + input[name="savenew"]').
+            // onclick...
+            click(function(){
+                // change form name from original to original_copy
+                $('input[name="name"]').attr('value', $('input[name="name"]').attr('value') + '_copy');
+            });
+    });
 
 js;
-		exit(0);
-	}";s:4:"help";s:301:"h1. upm_savenew
-
-Once installed and activated, you're ready to go.
-
-This plugin started as "a hack created by *grapeice925*":http://forum.textpattern.com/viewtopic.php?id=2586 of the Textpattern forum. I converted it into an admin-side plugin, and extended it to include a "Save New" button for forms.";s:8:"help_raw";s:301:"h1. upm_savenew
-
-Once installed and activated, you're ready to go.
-
-This plugin started as "a hack created by *grapeice925*":http://forum.textpattern.com/viewtopic.php?id=2586 of the Textpattern forum. I converted it into an admin-side plugin, and extended it to include a "Save New" button for forms.";s:15:"allow_html_help";i:0;s:3:"md5";s:32:"41ed3143534381dfd6a0f63d6c696a96";}
+        exit(0);
+    }?>
